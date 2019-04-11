b11ab5f6048ea0c95dbc7845ead5ba5ab9fad370
Marcio Camargo <x18169660@student.ncirl.ie>
2019-04-11 19:29:14 +0000

    home_controller.rb edited online with Bitbucket

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,8 @@
 class HomeController < ApplicationController
   require 'receipt_data_extraction'
   def index
-        @user = User.find(current_user.id)
+        if user_signed_in?
+           @user = User.find(current_user.id) 
+      end  
   end
 end
